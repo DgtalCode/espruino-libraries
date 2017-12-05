f72fbb13e4058bd7b4c5fc9969f71e801e2b1d5c
DanSpark1 <32205408+DanSpark1@users.noreply.github.com>
2017-12-05 17:15:54 +0300

    Update BlynkLib.js

--- BlynkLib.js
+++ BlynkLib.js
@@ -168,6 +168,7 @@ if (isEspruino()) {
       self.blynk = blynk;
     };
     this.process = function(values) {
+      console.log("      " + values);
       switch(values[0]) {
         case 'pm':
           // TODO
