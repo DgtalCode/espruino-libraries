f9f6300c7f071db59129854a700b1ab3d2eac338
DanSpark1 <32205408+DanSpark1@users.noreply.github.com>
2018-01-03 18:29:05 +0300

    1

---
+++ HW-SMH/examples/nodejs/client-with-bridge.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var Blynk = require('blynk-library');
+
+var AUTH = 'YOUR_AUTH_TOKEN';
+
+var blynk = new Blynk.Blynk(AUTH);
+
+var v1 = new blynk.VirtualPin(1);
+var v9 = new blynk.VirtualPin(9);
+var bridge1 = new blynk.WidgetBridge(31);
+
+v1.on('write', function(param) {
+  console.log('V1:', param);
+  bridge1.digitalWrite(13, param);
+});
+
+v9.on('read', function() {
+  v9.write(new Date().getSeconds());
+});
+
+blynk.on('connect', function() {
+  bridge1.setAuthToken('Your another api key');
+  console.log("Blynk ready.");
+});
+blynk.on('disconnect', function() { console.log("DISCONNECT"); });
