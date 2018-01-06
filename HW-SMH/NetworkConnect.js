7de7bcf4789b41dcd1dd17e2461cb4cb6dadb91a
DanSpark1 <32205408+DanSpark1@users.noreply.github.com>
2018-01-06 18:31:18 +0300

    1

--- HW-SMH/NetworkConnect.js
+++ HW-SMH/NetworkConnect.js
@@ -1,10 +1,8 @@
 var Net = function(){
 }
 
-exports.connect = function() {
+exports.connect = function(SSID, PSWD) {
 	PrimarySerial.setup(115200);
-	var SSID = 'interzet48';
-	var PSWD = '09876543210';
 	var wifi = require('@amperka/wifi').setup(PrimarySerial, function(err) {
 		// подключаемся к Wi-Fi сети
 		wifi.connect(SSID, PSWD, function(err) {
