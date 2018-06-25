64016136ac038aad978dac7d93dc83a13a423ab3
Python36 <enderdan80@gmail.com>
2018-06-25 16:13:47 +0300

    11

--- modules/WIFI_UDP/wifi.js
+++ modules/WIFI_UDP/wifi.js
@@ -8,7 +8,6 @@ var ESP = function(SSID, PSWD, UDP_HOST, UDP_PORT) {
 };
 
 exports.setup = function(usart) {
-  return new Promise((resolve, reject) => {
     ESP.at = require('AT');
-  });
+    return new ESP;
 }
